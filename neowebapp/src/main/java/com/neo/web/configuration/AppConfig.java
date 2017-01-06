86d8d80e36ed289059cd35ff2dad57fbe55285d3
erjun.guo@jince.com <erjun.guo@jince.com>
2017-01-06 21:39:02 +0800

    update

---
+++ neowebapp/src/main/java/com/neo/web/configuration/AppConfig.java
@@ -0,0 +1,19 @@
+package com.neo.web.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig {
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+}
